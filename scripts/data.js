1e7da42724f1e9980ce0ec5f0eb6b101660b414f
Eric Taylor <erictaylor@Erics-MacBook-Pro.local>
2019-07-25 15:30:31 -0500

    this is a test of the coding demigod system

---
+++ scripts/data.js
@@ -0,0 +1,16 @@
+const eric = (5 + 37)
+console.log(eric)
+
+
+// get/fetch data 
+// Referece to dom Element 
+// event listener
+// get Data
+// html representation
+// render to dom
+
+// const getAPIData = () => {
+//     fetch("https://https://data.nashville.gov/resource/74d7-b74t.json")
+//         .then(response => response.json()) //get the data
+
+// }
