64081cb6f7699c7bb93a6e3db543c068e9cf79a9
Sarah Fleming <sarah.k.fleming@gmail.com>
2019-07-26 09:32:02 -0500

    added API Keys File

---
+++ scripts/keys.js
@@ -0,0 +1,6 @@
+const apiKeys = {
+    eventBrite: "C4G2JEHUVJURWSVCMT",
+    metroParks: "6c1ji09gs1kp770qui0dwny61",
+    ticketMaster: "gPVzmUuph7mMvzSEDGAKHgroVfHxPPMk",
+    zomato: "38e78bdadcd293f73b93d4476b845664"
+}
