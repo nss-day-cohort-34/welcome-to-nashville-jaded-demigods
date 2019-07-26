362dce15031bba3a11461b2a9301fbc33e2b4fe1
Eric Taylor <erictaylor@Erics-MacBook-Pro.local>
2019-07-26 13:07:16 -0500

    successful fetch

--- scripts/data.js
+++ scripts/data.js
@@ -7,13 +7,8 @@
 
 
 const getParksData = () => {
-    fetch("https://data.nashville.gov/resource/74d7-b74t.json", {
-      "headers": {
-        "Content-Type": "application/json"
-      }
-    })
-      .then(response => response.json())
-      .then(results => {
-        console.log(results)
-      })
-  }
+    return fetch("https://data.nashville.gov/resource/74d7-b74t.json").then
+        (response => response.json())
+};
+console.log(getParksData())
+
