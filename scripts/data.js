388880f1cd4daf747991c200c6c4b818aad55309
Sarah Fleming <sarah.k.fleming@gmail.com>
2019-07-26 10:30:58 -0500

    got the ticketmaster data

--- scripts/data.js
+++ scripts/data.js
@@ -1,8 +1,20 @@
 const someCode = "here's some code"
-const fetchAPIData = () => {
-    fetch(`https://www.eventbriteapi.com/v3/events/search/?q=${searchString}&location.address=nashville&token=${token.name}`, {
-  "headers": {
+const fetchEventBriteData = () => {
+  return fetch(`https://www.eventbriteapi.com/v3/events/search/?q=${searchString}&location.address=nashville&token=${apiKeys.eventBrite}`, {
+    "headers": {
       "Accept": "application/json"
-  }
-})
+    }
+  })
+    .then(response => response.json())
 }
+// 343 = Nashville
+const fetchTicketMasterData = () => {
+  return fetch(`https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&dmaId=343&apikey=${apiKeys.ticketMaster}`)
+    .then(response => response.json())
+}
+
+fetchTicketMasterData()
+.then((events) => {
+  console.log(events)
+}
+  )
