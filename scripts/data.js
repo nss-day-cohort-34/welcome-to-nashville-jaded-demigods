0e36dc5cc473b0748988bbbce15eb15dc8938a90
Sarah Fleming <sarah.k.fleming@gmail.com>
2019-07-26 15:17:36 -0500

    fixing merge

--- scripts/data.js
+++ scripts/data.js
@@ -12,3 +12,16 @@ const fetchTicketMasterData = () => {
   return fetch(`https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&dmaId=343&classificationId=${genre}&daterange=from20190726-to-20191231&apikey=${apiKeys.ticketMaster}`)
     .then(response => response.json())
 }
+
+// get/fetch data 
+// Referece to dom Element 
+// event listener
+// get Data
+// html representation
+// render to dom
+
+const getParksData = () => {
+  return fetch("https://data.nashville.gov/resource/74d7-b74t.json").then
+      (response => response.json())
+};
+console.log(getParksData())
