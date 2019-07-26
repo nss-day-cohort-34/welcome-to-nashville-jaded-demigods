47968a2bf45c92ef60bf60792e42a8aa282d0cba
Jacquelyn M <jacquelynkmccray@gmail.com>
2019-07-26 15:11:42 -0500

    changed restaurants input to select price options

--- scripts/main.js
+++ scripts/main.js
@@ -1,42 +1,22 @@
 const restBtn = document.querySelector("#restaurants-btn")
-const restSearch = document.querySelector("#restaurants-input")
+const restPriceSelection = document.querySelector("#restaurants-input")
 let idNum = 1
 
-
-// const searchAPI = (arr, userInput) => {
-//   // const userInput = restSearch.value
-//   arr.find(userInput)
-
-// }
-
-// restBtn.addEventListener("click",)
-
-// var ages = [3, 10, 18, 20];
-
-// const checkAdult = (age) => {
-//   return age >= 18;
-// }
-
-// const myFunction = () => {
-//   document.getElementById("nash-container").innerHTML += ages.find(checkAdult);
-// }
-
-// myFunction()
-
-const userInput = "City"
-const searchAPI = (restaurantName) => {
-  return restaurantName >= userInput
+const searchPrices = (restaurantObj) => {
+  return restaurantObj.price === userInput
 }
 
 getRestaurantsData()
   .then((overallObj) => {
     const restaurantsArr = overallObj.restaurants
-    for (let i = 0; i < 4; i++) {
-      const restaurantObj = restaurantsArr.find(searchAPI)
-      const htmlString = resultsHTMLrep(restaurantObj.name, restaurantObj.address, idNum)
+    const resultsArr = restaurantsArr.filter(searchPrices)
+    for ( let i = 0; i < 4; i++ ){
+      const restaurant = resultsArr[i]
+      const htmlString = resultsHTMLrep(restaurant.name, restaurant.address, idNum)
       renderResults(htmlString)
       idNum++
-      restaurantsArr.splice(restaurantObj)
     }
   })
+
+
 
