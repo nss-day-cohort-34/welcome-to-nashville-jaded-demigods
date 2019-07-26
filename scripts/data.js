c2fe11890f0f36c08b76e14e34d965c8e27ef7a3
Eric Taylor <erictaylor@Erics-MacBook-Pro.local>
2019-07-26 09:30:52 -0500

    updated merged

--- scripts/data.js
+++ scripts/data.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-const eric = (5 + 37)
-console.log(eric)
-
-
 // get/fetch data 
 // Referece to dom Element 
 // event listener
@@ -11,23 +5,17 @@ console.log(eric)
 // html representation
 // render to dom
 
-// const getAPIData = () => {
-//     fetch("https://https://data.nashville.gov/resource/74d7-b74t.json")
-//         .then(response => response.json()) //get the data
 
-// }
-=======
-=======
-const someCode = "here's some code"
->>>>>>> 0f287391b30e9dbd833e47e2a8f6509e108ff7d8
-const fetchAPIData = () => {
-    fetch(`https://www.eventbriteapi.com/v3/events/search/?q=${searchString}&location.address=nashville&token=${token.name}`, {
-  "headers": {
-      "Accept": "application/json"
+const getParksData = () => {
+    fetch("https://data.nashville.gov/resource/74d7-b74t.json", {
+      "headers": {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(response => response.json())
+      .then(results => {
+        console.log(results)
+      })
   }
-}
-}
-<<<<<<< HEAD
->>>>>>> master
-=======
->>>>>>> 0f287391b30e9dbd833e47e2a8f6509e108ff7d8
+  
+  // getRestaurantsData()
