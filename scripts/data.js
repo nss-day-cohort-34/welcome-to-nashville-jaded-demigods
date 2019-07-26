fbff3141f1b207ff54015458d122670af15c3fee
Sarah Fleming <sarah.k.fleming@gmail.com>
2019-07-26 15:10:19 -0500

    data displayed. about to start working on user input

--- scripts/data.js
+++ scripts/data.js
@@ -9,14 +9,6 @@
 
 // 343 = Nashville
 const fetchTicketMasterData = () => {
-  return fetch(`https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&dmaId=343&apikey=${apiKeys.ticketMaster}`)
+  return fetch(`https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&dmaId=343&classificationId=${genre}&daterange=from20190726-to-20191231&apikey=${apiKeys.ticketMaster}`)
     .then(response => response.json())
-    
-const getRestaurantsData = () => {
-  return fetch("https://cors-anywhere.herokuapp.com/https://opentable.herokuapp.com/api/restaurants?city=Nashville&per_page=100", {
-    "headers": {
-      "Content-Type": "application/json",
-    }
-  })
-  .then(response => response.json())
 }
