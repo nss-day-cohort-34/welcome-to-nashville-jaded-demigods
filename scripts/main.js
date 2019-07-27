58b4d1ac1bf5e0f5ae5891a3e13a31024c3b51fd
Jacquelyn M <jacquelynkmccray@gmail.com>
2019-07-26 21:18:55 -0500

    Added fields for searching by restaurant name or address

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,29 @@
 const restBtn = document.querySelector("#restaurants-btn")
-const restPriceSelection = document.querySelector("#restaurants-input")
+const restPriceSelection = document.querySelector("#restaurants-select")
+const restNameSearch = document.querySelector("#restaurants-input")
+const restNameSearchBtn = document.querySelector("#rest-name-btn")
+const restStreetSearch = document.querySelector("#restaurant-street-input")
+const restStreetSearchBtn = document.querySelector("#rest-street-btn")
 let resultsContainer = document.querySelector("#results-list")
 let idNum = 1
-// const restaurantItinerary = document.querySelector("#itinerary--2")
 
 
 getRestaurantsData()
   .then((overallObj) => {
     const restaurantsArr = overallObj.restaurants
 
-    const searchForRestaurants = (optValue) => {
+    const search = (prop, userInput) => {
       const resultsArr = restaurantsArr.filter((restaurantObj) => {
-        return restaurantObj.price === optValue
+        if (prop == "price") {
+          return restaurantObj[prop] === userInput
+        } else {
+          return restaurantObj[prop].includes(userInput)
+        }
       })
-      if (resultsArr.length === 0) {
+      if (resultsArr.length === 0 && prop === "price") {
         alert("No restaurants in this price range")
+      } else if (resultsArr.length === 0 && prop === "name") {
+        alert("No restaurants with this name")
       } else {
         resultsArr.forEach(restaurantObj => {
           const htmlString = resultsHTMLrep(restaurantObj, idNum)
@@ -27,20 +36,36 @@ getRestaurantsData()
     restBtn.addEventListener("click", () => {
       resultsContainer.innerHTML = ""
       const optionValue = restPriceSelection.selectedIndex + 1
-      searchForRestaurants(optionValue)
+      search("price",optionValue)
     })
 
-    resultsContainer.addEventListener("click", () => {
-      if (event.target.id.includes("save--rest--")) {
-        const idNum = event.target.id.split("--")[2]
-        const liId = `li--${idNum}`
-        const li = document.querySelector(`#${liId}`)
-        const restName = li.textContent.split(":")[0]
-        addRestToItinerary(restName)
-      }
+    restNameSearchBtn.addEventListener("click", () => {
+      restPriceSelection.selectedIndex = 0
+      resultsContainer.innerHTML = ""
+      const nameSearch = restNameSearch.value
+      search("name",nameSearch)
+      restNameSearch.value = null
+    })
+    
+    restStreetSearchBtn.addEventListener("click", () => {
+      restPriceSelection.selectedIndex = 0
+      resultsContainer.innerHTML = ""
+      const streetSearch = restStreetSearch.value
+      search("address",streetSearch)
+      restStreetSearch.value = null
     })
 
-  })
+      resultsContainer.addEventListener("click", () => {
+        if (event.target.id.includes("save--rest--")) {
+          const idNum = event.target.id.split("--")[2]
+          const liId = `li--${idNum}`
+          const li = document.querySelector(`#${liId}`)
+          const restName = li.textContent.split(":")[0]
+          addRestToItinerary(restName)
+        }
+      })
+
+    })
 
 
 
