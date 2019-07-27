966cc0de9ed0942c848e19a009d9f2f0aab15042
Jacquelyn M <jacquelynkmccray@gmail.com>
2019-07-26 23:27:32 -0500

    further modularized code by extracting functions from .then

--- scripts/main.js
+++ scripts/main.js
@@ -8,12 +8,9 @@ const restStreetSearchBtn = document.querySelector("#rest-street-btn")
 let resultsContainer = document.querySelector("#results-list")
 let idNum = 1
 
+// Create array of restaurant objects based on user input. The filter method accepts a function and gives back an array.
+function searchData(arr, prop, userInput) {
 
-
-// Function to be included in .addEventListener for the search buttons
-function search(arr, prop, userInput) {
-
-  // Filter data from API based on user's search parameter (the filter method accepts a function and gives back an array)
   const resultsArr = arr.filter((restaurantObj) => {
     if (prop == "price") {
       return restaurantObj[prop] === userInput
@@ -21,73 +18,71 @@ function search(arr, prop, userInput) {
       return restaurantObj[prop].includes(userInput)
     }
   })
+  return resultsArr
+}
 
-  // If the user's search returned no results, alert the user
-  if (resultsArr.length === 0 && prop === "price") {
-    alert("No restaurants in this price range. Please search another price range.")
-  } else if (resultsArr.length === 0 && prop === "name") {
-    alert("No restaurants with this name. Please search for another name.")
-  } else if (resultsArr.length === 0 && prop === "address") {
-    alert("No restaurants with this address. Please search for street name.")
-
-    // If the user's search has results, invoke functions to create HTML representation and render it in the DOM
-  } else {
-    resultsArr.forEach(restaurantObj => {
-      const htmlString = resultsHTMLrep(restaurantObj, idNum)
-      renderResults(htmlString)
-
-      idNum++
-    })
+// Check if the user's input yields results. If the no results, alert the user. 
+function checkUserInput(arr, prop) {
+  if (arr.length === 0 && prop === "price") {
+    resultsContainer.innerHTML = "No restaurants in this price range. Please search another price range."
+  } else if (arr.length === 0 && prop === "name") {
+    resultsContainer.innerHTML = "No restaurants with this name. Please search for another name."
+  } else if (arr.length === 0 && prop === "address") {
+    resultsContainer.innerHTML = "No restaurants with this address. Please search for street name."
   }
 }
 
-// function addClickListener() {
-//   btn.addEventListener("click",() => {
+// If the user input passes the tests, invoke the functions to create the html representation and render the results in the DOM
+function showResults(arr) {
+  arr.forEach(restaurantObj => {
+    const htmlString = resultsHTMLrep(restaurantObj, idNum)
+    renderResults(htmlString)
+    idNum++
+  })
+}
 
-//   })
-// }
+// Add click listener to select element (intended for price options)
+function addClickListenerToSelect(btn, select, arr, prop) {
+  btn.addEventListener("click", () => {
+    resultsContainer.innerHTML = ""               // Reset inner html of results container
+    if (select.selectedIndex === 0) {
+      alert("Select a price range")
+      select.focus()
+      return
+    }
+    const optionValue = select.selectedIndex
+    const resultsArr = searchData(arr, prop, optionValue)
+    checkUserInput(resultsArr, prop)
+    showResults(resultsArr)
+  })
+}
 
-// Fetch the data from the API and add event listeners to each of the search buttons. Add event listener to the results container and 
+// Add click listener to input element (intended for name and address searches)
+function addClickListenerToTextInput(btn, input, type, arr, prop) {
+  btn.addEventListener("click", () => {
+    resultsContainer.innerHTML = ""                // Reset inner html of results container
+    if (input.value === "") {
+      alert(`Please enter a ${type} name`)
+      input.focus()
+      return
+    }
+    restPriceSelection.selectedIndex = 0           // Reset the price selection to none
+    const resultsArr = searchData(arr, prop, input.value)
+    checkUserInput(resultsArr, prop)
+    showResults(resultsArr)
+    input.value = null
+  })
+}
+
+// Fetch the data from the API and add event listeners to each of the search buttons. 
+// Add event listener to the results container. If save button is clicked, add corresponding restaurant name to itinerary.
 getRestaurantsData()
   .then((overallObj) => {
     const restaurantsArr = overallObj.restaurants
 
-    restPriceSearchBtn.addEventListener("click", () => {
-      if (restPriceSelection.selectedIndex === 0) { 
-        alert("Select a price range")
-        restPriceSelection.focus()
-        return 
-      }
-      resultsContainer.innerHTML = ""                // Reset inner html of results container
-      const optionValue = restPriceSelection.selectedIndex
-      search(restaurantsArr, "price", optionValue)
-    })
-
-    restNameSearchBtn.addEventListener("click", () => {
-      if (restNameSearch.value === "") {
-        alert("Please enter a restaurant name")
-        restNameSearch.focus()
-        return
-      }
-      restPriceSelection.selectedIndex = 0           // Reset the price selection to none
-      resultsContainer.innerHTML = ""                // Reset inner html of results container
-      const nameSearch = restNameSearch.value
-      search(restaurantsArr, "name", nameSearch)
-      restNameSearch.value = null                    // Empty the input
-    })
-
-    restStreetSearchBtn.addEventListener("click", () => {
-      if (restStreetSearch.value === "") {
-        alert("Please enter a street name")
-        restStreetSearch.focus()
-        return
-      }
-      restPriceSelection.selectedIndex = 0          // Reset the price selection to none
-      resultsContainer.innerHTML = ""               // Reset inner html of results container
-      const streetSearch = restStreetSearch.value
-      search(restaurantsArr, "address", streetSearch)
-      restStreetSearch.value = null                 // Empty the input
-    })
+    addClickListenerToSelect(restPriceSearchBtn, restPriceSelection, restaurantsArr, "price")
+    addClickListenerToTextInput(restNameSearchBtn, restNameSearch, "restaurant", restaurantsArr, "name")
+    addClickListenerToTextInput(restStreetSearchBtn, restStreetSearch, "street", restaurantsArr, "address")
 
     resultsContainer.addEventListener("click", () => {
       if (event.target.id.includes("save--rest--")) {
@@ -98,7 +93,6 @@ getRestaurantsData()
         addRestToItinerary(restName)
       }
     })
-
   })
 
 
