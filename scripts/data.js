5f6226ea7e4700027b909ee882ddde1b8b9236ab
Sarah Fleming <sarah.k.fleming@gmail.com>
2019-07-28 11:30:14 -0500

    still working - no important changes

--- scripts/data.js
+++ scripts/data.js
@@ -7,11 +7,11 @@
 // render to dom
 
 
-const getParksData = () => {
-    return fetch("https://data.nashville.gov/resource/74d7-b74t.json").then
-        (response => response.json())
-};
-console.log(getParksData())
+// const getParksData = () => {
+//     return fetch("https://data.nashville.gov/resource/74d7-b74t.json").then
+//         (response => response.json())
+// };
+// console.log(getParksData())
 
 // const fetchEventBriteData = () => {
 //   return fetch(`https://www.eventbriteapi.com/v3/events/search/?q=${searchString}&location.address=nashville&token=${apiKeys.eventBrite}`, {
--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,25 @@
 // Save button selected
 // document.querySelector(`#save--${idNum}`)
 
+const genreOptions = {
+    alternative: "KnvZfZ7vAvv",
+    blues: "KnvZfZ7vAvd",
+    country: "KnvZfZ7vAv6",
+    folk: "KnvZfZ7vAva",
+    hipHopRap: "KnvZfZ7vAv1",
+    pop: "KnvZfZ7vAev",
+    rock: "KnvZfZ7vAeA",
+} 
+
 // Get user input
 const concertsButton = document.querySelector("#concerts-btn").addEventListener("click", event => {
   // Search Genre
-    const genre = document.querySelector("#music-genres")
+    const genre = document.querySelector("#music-genres").value
+    // let selectedGenre = genreSelector.options[genreSelector.selectedIndex].text
+    console.log(genre)
     // Look up the selectedIndex property. Create an object with music genres and IDs and iterate through that instead of trying to assign the Genre ID value to the value of the select option. Figure out which one is easier. 
 })
+
 
 
 fetchTicketMasterData()
@@ -25,4 +38,5 @@ fetchTicketMasterData()
             const eventHTML = createEventHTML(id, event, venueName)
             renderEvent(eventHTML)
         }
+        
     })
