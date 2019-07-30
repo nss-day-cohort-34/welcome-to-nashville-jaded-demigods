15fd5a53ec00e79bf3833f538b201cbf5bdf00ee
Sarah Fleming <sarah.k.fleming@gmail.com>
2019-07-30 08:49:11 -0500

    added some comments

--- scripts/main-concerts.js
+++ scripts/main-concerts.js
@@ -1,16 +1,14 @@
-// Get reference to genre options use can select in index.html
+// Get reference to genre options user can select in index.html
 const genre = document.querySelector("#music-genres")
 
 // Get reference to concerts search button
 const concertsSearchButton = document.querySelector("#concerts-btn")
 
-// Get reference to save button next to a concert item
-//const saveConcertItem = document.querySelector(`#save--${idNum}`)
-
 concertsSearchButton.addEventListener("click", () => {
     // Call fetch function and pass in user's selected genre
     fetchTicketMasterData(genre.value)
         .then((overallObject) => {
+            // Set HTML in results container to empty string before returning search results 
             concertsContainer.innerHTML = ""
             let id = 0
             let venueName = ""
@@ -26,17 +24,17 @@ concertsSearchButton.addEventListener("click", () => {
                 const concertsHTML = createConcertHTML(id, event, venueName)
                 renderConcerts(concertsHTML)
             }
-                concertsContainer.addEventListener("click", () => {
-                    if (event.target.id.includes("save-concert--")) {
-                        const getConcertIdNum = event.target.id.split("--")[1]
-                        const getConcertNameId = `concert-name--${getConcertIdNum}`
-                        const getConcertName = document.querySelector(`#${getConcertNameId}`).textContent
-                        addConcertToItinerary(getConcertName)
+            concertsContainer.addEventListener("click", () => {
+                if (event.target.id.includes("save-concert--")) {
+                    const getConcertIdNum = event.target.id.split("--")[1]
+                    const getConcertNameId = `concert-name--${getConcertIdNum}`
+                    const getConcertName = document.querySelector(`#${getConcertNameId}`).textContent
+                    addConcertToItinerary(getConcertName)
 
-                    } else {
-                        return
-                    }
-                })
+                } else {
+                    return
+                }
+            })
 
         })
 })
